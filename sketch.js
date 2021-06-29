ee582701b689e8dc28547dd64ec4eab8b80afc3f
sunakshiwhjr <77574525+sunakshiwhjr@users.noreply.github.com>
2021-06-29 14:36:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200, 200, 30, 30);
+  box.shapeColor = "Teal";
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown("right"))
+ {
+    box.x = box.x + 3;
+
+ }
+
+ if(keyDown("left"))
+ {
+    box.position.x = box.position.x - 3;
+ }
+
+ if(keyDown("up"))
+ {
+   box.y = box.y -3;
+
+ }
+
+ if(keyDown("down"))
+ {
+   box.y = box.y +3;
+   
+ }
+  drawSprites();
+
+
+}
+
+
+
+
